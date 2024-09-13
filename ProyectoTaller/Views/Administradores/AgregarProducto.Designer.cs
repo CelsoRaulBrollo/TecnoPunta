8d98f0902644e1a6a0480be61c8503f724c6ed66
Celso Raul Brollo <celsobrollo@gmail.com>
2024-09-12 22:09:44 -0300

    Validaciones Agregar Producto

--- ProyectoTaller/Views/Administradores/AgregarProducto.Designer.cs
+++ ProyectoTaller/Views/Administradores/AgregarProducto.Designer.cs
@@ -42,6 +42,13 @@
             this.BBorrar = new System.Windows.Forms.Button();
             this.BAgregar = new System.Windows.Forms.Button();
             this.PBImagen = new System.Windows.Forms.PictureBox();
+            this.LValiNombre = new System.Windows.Forms.TextBox();
+            this.LValiPrecio = new System.Windows.Forms.TextBox();
+            this.LValiCate = new System.Windows.Forms.TextBox();
+            this.LValiStock = new System.Windows.Forms.TextBox();
+            this.LValido = new System.Windows.Forms.TextBox();
+            this.BAgregarImagen = new System.Windows.Forms.Button();
+            this.LValiImagen = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.PBImagen)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,7 +56,7 @@
             // 
             this.LTituloAgregar.AutoSize = true;
             this.LTituloAgregar.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LTituloAgregar.Location = new System.Drawing.Point(281, 23);
+            this.LTituloAgregar.Location = new System.Drawing.Point(281, 25);
             this.LTituloAgregar.Name = "LTituloAgregar";
             this.LTituloAgregar.Size = new System.Drawing.Size(219, 28);
             this.LTituloAgregar.TabIndex = 0;
@@ -57,7 +64,7 @@
             // 
             // TNombreProducto
             // 
-            this.TNombreProducto.Location = new System.Drawing.Point(153, 73);
+            this.TNombreProducto.Location = new System.Drawing.Point(153, 96);
             this.TNombreProducto.Name = "TNombreProducto";
             this.TNombreProducto.Size = new System.Drawing.Size(205, 20);
             this.TNombreProducto.TabIndex = 1;
@@ -67,7 +74,7 @@
             // 
             this.LNombreProducto.AutoSize = true;
             this.LNombreProducto.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LNombreProducto.Location = new System.Drawing.Point(72, 76);
+            this.LNombreProducto.Location = new System.Drawing.Point(72, 99);
             this.LNombreProducto.Name = "LNombreProducto";
             this.LNombreProducto.Size = new System.Drawing.Size(61, 17);
             this.LNombreProducto.TabIndex = 2;
@@ -77,7 +84,7 @@
             // 
             this.LPrecio.AutoSize = true;
             this.LPrecio.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LPrecio.Location = new System.Drawing.Point(72, 129);
+            this.LPrecio.Location = new System.Drawing.Point(72, 152);
             this.LPrecio.Name = "LPrecio";
             this.LPrecio.Size = new System.Drawing.Size(48, 17);
             this.LPrecio.TabIndex = 3;
@@ -87,7 +94,7 @@
             // 
             this.LCategoria.AutoSize = true;
             this.LCategoria.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LCategoria.Location = new System.Drawing.Point(72, 181);
+            this.LCategoria.Location = new System.Drawing.Point(72, 204);
             this.LCategoria.Name = "LCategoria";
             this.LCategoria.Size = new System.Drawing.Size(75, 17);
             this.LCategoria.TabIndex = 4;
@@ -97,7 +104,7 @@
             // 
             this.LStock.AutoSize = true;
             this.LStock.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LStock.Location = new System.Drawing.Point(72, 232);
+            this.LStock.Location = new System.Drawing.Point(72, 255);
             this.LStock.Name = "LStock";
             this.LStock.Size = new System.Drawing.Size(43, 17);
             this.LStock.TabIndex = 5;
@@ -105,7 +112,7 @@
             // 
             // TPrecio
             // 
-            this.TPrecio.Location = new System.Drawing.Point(153, 126);
+            this.TPrecio.Location = new System.Drawing.Point(153, 149);
             this.TPrecio.Name = "TPrecio";
             this.TPrecio.Size = new System.Drawing.Size(205, 20);
             this.TPrecio.TabIndex = 6;
@@ -114,23 +121,25 @@
             // CBCategoria
             // 
             this.CBCategoria.FormattingEnabled = true;
-            this.CBCategoria.Location = new System.Drawing.Point(153, 177);
+            this.CBCategoria.Location = new System.Drawing.Point(153, 200);
             this.CBCategoria.Name = "CBCategoria";
             this.CBCategoria.Size = new System.Drawing.Size(205, 21);
             this.CBCategoria.TabIndex = 7;
+            this.CBCategoria.SelectedIndexChanged += new System.EventHandler(this.CBCategoria_SelectedIndexChanged);
             // 
             // TStock
             // 
-            this.TStock.Location = new System.Drawing.Point(153, 229);
+            this.TStock.Location = new System.Drawing.Point(153, 252);
             this.TStock.Name = "TStock";
             this.TStock.Size = new System.Drawing.Size(205, 20);
             this.TStock.TabIndex = 8;
+            this.TStock.TextChanged += new System.EventHandler(this.TStock_TextChanged);
             // 
             // LImagen
             // 
             this.LImagen.AutoSize = true;
             this.LImagen.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LImagen.Location = new System.Drawing.Point(72, 284);
+            this.LImagen.Location = new System.Drawing.Point(72, 307);
             this.LImagen.Name = "LImagen";
             this.LImagen.Size = new System.Drawing.Size(58, 17);
             this.LImagen.TabIndex = 9;
@@ -143,13 +152,14 @@
             this.BSalir.Font = new System.Drawing.Font("Century", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BSalir.Image = global::ProyectoTaller.Properties.Resources.Salir;
             this.BSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.BSalir.Location = new System.Drawing.Point(543, 392);
+            this.BSalir.Location = new System.Drawing.Point(543, 415);
             this.BSalir.Name = "BSalir";
             this.BSalir.Size = new System.Drawing.Size(114, 52);
             this.BSalir.TabIndex = 13;
             this.BSalir.Text = "Salir       ";
             this.BSalir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.BSalir.UseVisualStyleBackColor = true;
+            this.BSalir.Click += new System.EventHandler(this.BSalir_Click);
             // 
             // BBorrar
             // 
@@ -161,13 +171,14 @@
             this.BBorrar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.BBorrar.Image = global::ProyectoTaller.Properties.Resources.Eliminar;
             this.BBorrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.BBorrar.Location = new System.Drawing.Point(543, 251);
+            this.BBorrar.Location = new System.Drawing.Point(543, 274);
             this.BBorrar.Name = "BBorrar";
             this.BBorrar.Size = new System.Drawing.Size(114, 52);
             this.BBorrar.TabIndex = 12;
             this.BBorrar.Text = "Borrar Todo";
             this.BBorrar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.BBorrar.UseVisualStyleBackColor = true;
+            this.BBorrar.Click += new System.EventHandler(this.BBorrar_Click);
             // 
             // BAgregar
             // 
@@ -176,33 +187,147 @@
             this.BAgregar.Font = new System.Drawing.Font("Century", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BAgregar.Image = global::ProyectoTaller.Properties.Resources.Disco;
             this.BAgregar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.BAgregar.Location = new System.Drawing.Point(543, 321);
+            this.BAgregar.Location = new System.Drawing.Point(543, 344);
             this.BAgregar.Name = "BAgregar";
             this.BAgregar.Size = new System.Drawing.Size(114, 52);
             this.BAgregar.TabIndex = 11;
             this.BAgregar.Text = "Agregar    ";
             this.BAgregar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.BAgregar.UseVisualStyleBackColor = true;
+            this.BAgregar.Click += new System.EventHandler(this.BAgregar_Click);
             // 
             // PBImagen
             // 
             this.PBImagen.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.PBImagen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.PBImagen.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.PBImagen.Image = global::ProyectoTaller.Properties.Resources.SubirImg;
-            this.PBImagen.Location = new System.Drawing.Point(153, 284);
+            this.PBImagen.Location = new System.Drawing.Point(153, 307);
             this.PBImagen.Name = "PBImagen";
             this.PBImagen.Size = new System.Drawing.Size(205, 152);
             this.PBImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.PBImagen.TabIndex = 10;
             this.PBImagen.TabStop = false;
+            this.PBImagen.Click += new System.EventHandler(this.PBImagen_Click);
+            // 
+            // LValiNombre
+            // 
+            this.LValiNombre.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LValiNombre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LValiNombre.Enabled = false;
+            this.LValiNombre.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LValiNombre.ForeColor = System.Drawing.Color.Firebrick;
+            this.LValiNombre.Location = new System.Drawing.Point(153, 122);
+            this.LValiNombre.Multiline = true;
+            this.LValiNombre.Name = "LValiNombre";
+            this.LValiNombre.ReadOnly = true;
+            this.LValiNombre.Size = new System.Drawing.Size(205, 21);
+            this.LValiNombre.TabIndex = 20;
+            this.LValiNombre.TabStop = false;
+            this.LValiNombre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // LValiPrecio
+            // 
+            this.LValiPrecio.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LValiPrecio.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LValiPrecio.Enabled = false;
+            this.LValiPrecio.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LValiPrecio.ForeColor = System.Drawing.Color.Firebrick;
+            this.LValiPrecio.Location = new System.Drawing.Point(153, 175);
+            this.LValiPrecio.Multiline = true;
+            this.LValiPrecio.Name = "LValiPrecio";
+            this.LValiPrecio.ReadOnly = true;
+            this.LValiPrecio.Size = new System.Drawing.Size(205, 21);
+            this.LValiPrecio.TabIndex = 21;
+            this.LValiPrecio.TabStop = false;
+            this.LValiPrecio.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // LValiCate
+            // 
+            this.LValiCate.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LValiCate.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LValiCate.Enabled = false;
+            this.LValiCate.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LValiCate.ForeColor = System.Drawing.Color.Firebrick;
+            this.LValiCate.Location = new System.Drawing.Point(153, 227);
+            this.LValiCate.Multiline = true;
+            this.LValiCate.Name = "LValiCate";
+            this.LValiCate.ReadOnly = true;
+            this.LValiCate.Size = new System.Drawing.Size(205, 21);
+            this.LValiCate.TabIndex = 22;
+            this.LValiCate.TabStop = false;
+            this.LValiCate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // LValiStock
+            // 
+            this.LValiStock.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LValiStock.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LValiStock.Enabled = false;
+            this.LValiStock.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LValiStock.ForeColor = System.Drawing.Color.Firebrick;
+            this.LValiStock.Location = new System.Drawing.Point(153, 280);
+            this.LValiStock.Multiline = true;
+            this.LValiStock.Name = "LValiStock";
+            this.LValiStock.ReadOnly = true;
+            this.LValiStock.Size = new System.Drawing.Size(205, 21);
+            this.LValiStock.TabIndex = 23;
+            this.LValiStock.TabStop = false;
+            this.LValiStock.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // LValido
+            // 
+            this.LValido.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LValido.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LValido.Enabled = false;
+            this.LValido.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.LValido.ForeColor = System.Drawing.Color.Chartreuse;
+            this.LValido.Location = new System.Drawing.Point(278, 56);
+            this.LValido.Multiline = true;
+            this.LValido.Name = "LValido";
+            this.LValido.ReadOnly = true;
+            this.LValido.Size = new System.Drawing.Size(230, 34);
+            this.LValido.TabIndex = 24;
+            this.LValido.TabStop = false;
+            this.LValido.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BAgregarImagen
+            // 
+            this.BAgregarImagen.Location = new System.Drawing.Point(364, 307);
+            this.BAgregarImagen.Name = "BAgregarImagen";
+            this.BAgregarImagen.Size = new System.Drawing.Size(98, 30);
+            this.BAgregarImagen.TabIndex = 25;
+            this.BAgregarImagen.Text = "Agregar Imagen";
+            this.BAgregarImagen.UseVisualStyleBackColor = true;
+            this.BAgregarImagen.Click += new System.EventHandler(this.BAgregarImagen_Click);
+            // 
+            // LValiImagen
+            // 
+            this.LValiImagen.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LValiImagen.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LValiImagen.Enabled = false;
+            this.LValiImagen.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LValiImagen.ForeColor = System.Drawing.Color.Firebrick;
+            this.LValiImagen.Location = new System.Drawing.Point(153, 465);
+            this.LValiImagen.Multiline = true;
+            this.LValiImagen.Name = "LValiImagen";
+            this.LValiImagen.ReadOnly = true;
+            this.LValiImagen.Size = new System.Drawing.Size(205, 21);
+            this.LValiImagen.TabIndex = 26;
+            this.LValiImagen.TabStop = false;
+            this.LValiImagen.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // AgregarProducto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(800, 501);
+            this.ClientSize = new System.Drawing.Size(800, 506);
+            this.Controls.Add(this.LValiImagen);
+            this.Controls.Add(this.BAgregarImagen);
+            this.Controls.Add(this.LValido);
+            this.Controls.Add(this.LValiStock);
+            this.Controls.Add(this.LValiCate);
+            this.Controls.Add(this.LValiPrecio);
+            this.Controls.Add(this.LValiNombre);
             this.Controls.Add(this.BSalir);
             this.Controls.Add(this.BBorrar);
             this.Controls.Add(this.BAgregar);
@@ -243,5 +368,12 @@
         private System.Windows.Forms.Button BAgregar;
         private System.Windows.Forms.Button BBorrar;
         private System.Windows.Forms.Button BSalir;
+        private System.Windows.Forms.TextBox LValiNombre;
+        private System.Windows.Forms.TextBox LValiPrecio;
+        private System.Windows.Forms.TextBox LValiCate;
+        private System.Windows.Forms.TextBox LValiStock;
+        private System.Windows.Forms.TextBox LValido;
+        private System.Windows.Forms.Button BAgregarImagen;
+        private System.Windows.Forms.TextBox LValiImagen;
     }
 }
--- ProyectoTaller/Views/Administradores/AgregarProducto.cs
+++ ProyectoTaller/Views/Administradores/AgregarProducto.cs
@@ -17,14 +17,139 @@ namespace ProyectoTaller.Views.Administradores
             InitializeComponent();
         }
 
+        // Evento del botón "Agregar"
+        private void BAgregar_Click(object sender, EventArgs e)
+        {
+            string nombreProducto = TNombreProducto.Text;
+            string precioTexto = TPrecio.Text;
+            string stockTexto = TStock.Text;
+            string categoria = CBCategoria.SelectedItem?.ToString();
+
+            // Validaciones
+            bool valido = true;
+
+            if (string.IsNullOrWhiteSpace(nombreProducto))
+            {
+                LValiNombre.Text = "Ingrese el nombre del producto.";
+                valido = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(precioTexto) || !decimal.TryParse(precioTexto, out decimal precio) || precio <= 0)
+            {
+                LValiPrecio.Text = "Ingrese un precio válido.";
+                LValiPrecio.ForeColor = Color.Red;
+                valido = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(stockTexto) || !int.TryParse(stockTexto, out int stock) || stock < 0)
+            {
+                LValiStock.Text = "Ingrese un stock válido.";
+                LValiStock.ForeColor = Color.Red;
+                valido = false;
+            }
+
+            if (string.IsNullOrEmpty(categoria))
+            {
+                LValiCate.Text = "Seleccione una categoría.";
+                LValiCate.ForeColor = Color.Red;
+                valido = false;
+            }
+
+            // Validación de la imagen en el PictureBox
+            if (PBImagen.Image == null)
+            {
+                LValiImagen.Text = "Por favor, agregue una imagen.";
+                LValiImagen.ForeColor = Color.Red;
+                valido = false;
+            }
+
+            if (valido)
+            {
+                LValido.Text = "Producto agregado exitosamente.";
+                LValido.ForeColor = Color.Green;
+            }
+        }
+
+        private void BBorrar_Click(object sender, EventArgs e)
+        {
+            TNombreProducto.Clear();
+            TPrecio.Clear();
+            TStock.Clear();
+            CBCategoria.SelectedIndex = -1;
+            PBImagen.Image = null;
+
+            LValido.Text = string.Empty;
+            LimpiarMensajesDeValidacion();
+        }
+
+        private void LimpiarMensajesDeValidacion()
+        {
+            LValiNombre.Text = string.Empty;
+            LValiPrecio.Text = string.Empty;
+            LValiStock.Text = string.Empty;
+            LValiCate.Text = string.Empty;
+            LValiImagen.Text = string.Empty;
+        }
+
         private void TNombreProducto_TextChanged(object sender, EventArgs e)
         {
-
+            if (!string.IsNullOrWhiteSpace(TNombreProducto.Text))
+            {
+                LValiNombre.Text = string.Empty;
+            }
         }
 
         private void TPrecio_TextChanged(object sender, EventArgs e)
         {
+            if (decimal.TryParse(TPrecio.Text, out decimal precio) && precio > 0)
+            {
+                LValiPrecio.Text = string.Empty;
+            }
+        }
 
+        private void TStock_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(TStock.Text, out int stock) && stock >= 0)
+            {
+                LValiStock.Text = string.Empty;
+            }
+        }
+
+        private void CBCategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (CBCategoria.SelectedIndex != -1)
+            {
+                LValiCate.Text = string.Empty;
+            }
+        }
+
+        private void PBImagen_Click(object sender, EventArgs e)
+        {
+        }
+
+        // Evento del botón "Salir"
+        private void BSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Evento del botón "Agregar Imagen"
+        private void BAgregarImagen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Seleccione una imagen",
+                Filter = "Archivos de imagen (*.jpg; *.jpeg; *.png; *.bmp)|*.jpg;*.jpeg;*.png;*.bmp"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                PBImagen.Image = Image.FromFile(openFileDialog.FileName);
+
+                PBImagen.SizeMode = PictureBoxSizeMode.Zoom;
+
+                LValiImagen.Text = string.Empty;
+            }
         }
     }
 }
