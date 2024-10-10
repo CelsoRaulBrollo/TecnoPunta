b6a6c9470c39731760e8a96f2155dd1f71175948
Gabriel <capaygabriel98@gmail.com>
2024-10-10 12:29:01 -0300

    Modelo-Entidad Usuario

---
+++ ProyectoTaller/CModelos/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CModelos
+{
+    internal class Usuario
+    {
+        public int DNI_Usuario { get; set; }
+        public string Nombre_Usuario { get; set; }
+        public string Apellido_Usuario { get; set; }
+        public string Telefono_Usuario { get; set; }
+        public string Correo_Usuario { get; set; }
+        public string Direccion_Usuario { get; set; }
+
+        public Sexo Sexo_Usuario { get; set; }
+        public Rol Rol_Usuario { get; set; }
+
+    }
+}
