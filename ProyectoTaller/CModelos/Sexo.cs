352d8cf4553ea352ebc735581c88aaf9ef477ffc
Gabriel <capaygabriel98@gmail.com>
2024-10-10 12:28:51 -0300

    Modelo-Entidad Sexo

---
+++ ProyectoTaller/CModelos/Sexo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CModelos
+{
+    internal class Sexo
+    {
+        public int Id_Sexo { get; set; }
+        public string Descripcion_Sexo {  get; set; }
+    }
+}
