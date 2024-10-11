cc2c029356ec995efa3beecd384c400a12cc545c
Gabriel <capaygabriel98@gmail.com>
2024-10-11 14:24:58 -0300

    Add Capa Negocio de la Entidad Rol

---
+++ ProyectoTaller/CNegocio/RolNegocio.cs
@@ -0,0 +1,29 @@
+﻿using ProyectoTaller.CDatos;
+using ProyectoTaller.CModelos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CNegocio
+{
+    public class RolNegocio
+    {
+        private RolDatos rolDatos;
+
+        public List<Rol> ListarRoles()
+        {
+            rolDatos = new RolDatos();
+            return rolDatos.ObtenerRoles();
+        }
+
+        public int buscarIDRolPorDescripcion(string descripcion)
+        {
+            rolDatos = new RolDatos();
+            return rolDatos.buscarIDRolPorDescripcion(descripcion);
+        }
+
+    }
+}
