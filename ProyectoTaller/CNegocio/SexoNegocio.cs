0ed132989d7a04c62230185d23fefb84219b3d42
Gabriel <capaygabriel98@gmail.com>
2024-10-11 14:25:06 -0300

    Add Capa Negocio de la Entidad Sexo

---
+++ ProyectoTaller/CNegocio/SexoNegocio.cs
@@ -0,0 +1,28 @@
+﻿using ProyectoTaller.CDatos;
+using ProyectoTaller.CModelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CNegocio
+{
+    public class SexoNegocio
+    {
+
+        private SexoDato datos;
+
+        public List<Sexo> ListarSexo()
+        {
+            datos = new SexoDato();
+            return datos.ObtenerSexo();
+        }
+
+        public int buscarIDSexoPorDescripcion(string descripcion)
+        {
+            datos = new SexoDato();
+            return datos.buscarIDSexoPorDescripcion(descripcion);
+        }
+    }
+}
