ad694f8dd30844668f8e8e5e5931ceffb06ac1fc
Gabriel <capaygabriel98@gmail.com>
2024-10-12 15:44:47 -0300

    Add Capa Datos de la Entidad Condicion

---
+++ ProyectoTaller/CDatos/CondicionDatos.cs
@@ -0,0 +1,40 @@
+﻿using ProyectoTaller.CModelos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CDatos
+{
+    public class CondicionDatos
+    {
+        private ConexionBD conexion = new ConexionBD();
+
+        public List<Condicion> ObtenerMarcas()
+        {
+            List<Condicion> condicion = new List<Condicion>();
+
+            using (SqlConnection connection = conexion.ObtenerConexion())
+            {
+                string query = "SELECT Id_Condicion, Descripcion_Condicion FROM Condicion";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    condicion.Add(new Condicion
+                    {
+                        Id_Condicion = (int)reader["Id_Marca"],
+                        Descripcion_Condicion = reader["Descripcion_Marca"].ToString()
+                    });
+                }
+            }
+
+            return condicion;
+        }
+    }
+}
