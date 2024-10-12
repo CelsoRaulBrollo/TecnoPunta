068ed2d26951033bfa4443cf12155bd076be4c9e
Gabriel <capaygabriel98@gmail.com>
2024-10-12 15:36:28 -0300

    Add Capa Negocio de la Entidad Condicion

---
+++ ProyectoTaller/CModelos/Condicion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CModelos
+{
+    public class Condicion
+    {
+        public int Id_Condicion { get; set; }
+        public string Condicion_Rol { get; set; }
+    }
+}
