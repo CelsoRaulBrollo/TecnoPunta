d404b5ee74ddadd3be13b5f6274d9a9ee5ca8181
Gabriel <capaygabriel98@gmail.com>
2024-10-12 15:44:57 -0300

    Add Capa Datos de la Entidad Marca

---
+++ ProyectoTaller/CDatos/MarcaDatos.cs
@@ -0,0 +1,40 @@
+﻿using ProyectoTaller.CModelos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CDatos
+{
+    public class MarcaDatos
+    {
+        private ConexionBD conexion = new ConexionBD();
+
+        public List<Marca> ObtenerMarcas()
+        {
+            List<Marca> marcas = new List<Marca>();
+
+            using (SqlConnection connection = conexion.ObtenerConexion())
+            {
+                string query = "SELECT Id_Marcal, Descripcion_Marca FROM Marca"; 
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    marcas.Add(new Marca
+                    {
+                        Id_Marca = (int)reader["Id_Marca"],
+                        Descripcion_Marca = reader["Descripcion_Marca"].ToString()
+                    });
+                }
+            }
+
+            return marcas;
+        }
+    }
+}
