e0ff0b11c3e2d621adf838e2f0b537b44b6fc265
Gabriel <capaygabriel98@gmail.com>
2024-10-12 15:36:38 -0300

    Add Capa Negocio de la Entidad Marca

---
+++ ProyectoTaller/CModelos/Marca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CModelos
+{
+    public class Marca
+    {
+        public int Id_Marca { get; set; }
+        public string Marca_Rol { get; set; }
+    }
+}
