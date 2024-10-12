c4fb5abc2e0a44d4f82ab82a9651d48354df9dff
Gabriel <capaygabriel98@gmail.com>
2024-10-12 15:53:01 -0300

    Add Capa Negocio de la Entidad Condicion

---
+++ ProyectoTaller/CNegocio/CondicionNegocio.cs
@@ -0,0 +1,23 @@
+﻿using ProyectoTaller.CDatos;
+using ProyectoTaller.CModelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CNegocio
+{
+    public class CondicionNegocio
+    {
+        private CondicionDatos CondicionDatos;
+
+
+        public List<Condicion> ListarCondiciones()
+        {
+            CondicionDatos = new CondicionDatos();
+            return CondicionDatos.ObtenerCondiciones();
+        }
+
+    }
+}
