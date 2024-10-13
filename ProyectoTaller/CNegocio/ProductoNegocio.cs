f71d44d8665bdcde8f7326da5bc2a5fd4a761dea
Gabriel <capaygabriel98@gmail.com>
2024-10-12 22:00:35 -0300

    Implementacion

--- ProyectoTaller/CNegocio/ProductoNegocio.cs
+++ ProyectoTaller/CNegocio/ProductoNegocio.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ProyectoTaller.CDatos;
+using ProyectoTaller.CModelos;
+using ProyectoTaller.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,45 @@ namespace ProyectoTaller.CNegocio
 {
     public class ProductoNegocio
     {
+        private ProductoDatos productosDatos;
+        public List<ProductoDTO> listarProductos() { 
+            List<ProductoDTO> listaProductos = new List<ProductoDTO>();
+            productosDatos = new ProductoDatos();
+
+            foreach (var p in productosDatos.ObtenerProductos())
+            {
+                ProductoDTO productoDTO = new ProductoDTO
+                {
+                    Modelo = p.Modelo_Producto,
+                    Nombre = p.Nombre_Producto,
+                    SistemaOperativo = p.SistemaOperativo_Producto,
+                    Almacenamiento = p.Almacenamiento_Producto,
+                    Ram = p.Ram_Producto,
+                    Precio = p.Precio_Producto,
+                    Stock = p.Stock_Producto,
+                    Marca = p.Marca?.Nombre_Marca,
+                    Condicion = p.Condicion?.Descripcion_Condicion
+                };
+
+                listaProductos.Add(productoDTO); 
+            }
+
+            return listaProductos;
+
+        }
+
+        public void actualizarProducto(Producto producto)
+        {
+            productosDatos = new ProductoDatos();
+            productosDatos.ActualizarProducto(producto);   
+
+        }
+
+        public void guardarProducto(Producto producto)
+        {
+            productosDatos = new ProductoDatos();
+            productosDatos.guardarProducto(producto);
+            
+        }
     }
 }
