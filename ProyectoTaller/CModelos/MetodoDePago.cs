b74727fe1bb9b49d76f83e546a894a8142e68407
Gabriel <capaygabriel98@gmail.com>
2024-10-16 10:32:19 -0300

    ADD MetodoDePago

---
+++ ProyectoTaller/CModelos/MetodoDePago.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CModelos
+{
+    public class MetodoDePago
+    {
+        public int Id_MetodoDePago {  get; set; }
+        public string Descripcion_MetodoDePago { get; set; }
+    }
+}
