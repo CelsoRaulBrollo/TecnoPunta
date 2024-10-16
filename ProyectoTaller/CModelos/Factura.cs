a43c4c2e80a65531f30c6b781e1f7aa5820a42f6
Gabriel <capaygabriel98@gmail.com>
2024-10-16 10:32:03 -0300

    ADD Modelo Factura

---
+++ ProyectoTaller/CModelos/Factura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CModelos
+{
+    public class Factura
+    {
+        public int ID_Factura { get; set; }
+        public Clientes Cliente { get; set; }
+        public Usuario Vendedor { get; set; }
+        public DateTime FechaFacturacion { get; set; }
+        public MetodoDePago MetodoDePago { get; set; }
+        public int Total { get; set; }
+    }
+}
