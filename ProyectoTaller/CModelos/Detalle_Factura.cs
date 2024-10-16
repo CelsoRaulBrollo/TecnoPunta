a01412bb2122c37ab1daec155d315b464f02ec23
Gabriel <capaygabriel98@gmail.com>
2024-10-16 10:31:47 -0300

    ADD Modelo Detalle Factura

---
+++ ProyectoTaller/CModelos/Detalle_Factura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CModelos
+{
+    public class Detalle_Factura
+    {
+        public int ID_Detalle { get; set; }
+        public int Cantidad { get; set; }
+
+        public decimal Subtotal { get; set; }
+        public Producto Modelo { get; set; }
+       
+       
+    }
+}
