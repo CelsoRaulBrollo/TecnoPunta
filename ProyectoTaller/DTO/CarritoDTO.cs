ed8ca28a9eeb3ba4cfdb9ec098d73fd3f6957f83
Gabriel <capaygabriel98@gmail.com>
2024-10-23 22:22:29 -0300

    Plantilla Carrito

---
+++ ProyectoTaller/DTO/CarritoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.DTO
+{
+    public class CarritoDTO
+    {
+        public decimal total {  get; set; }
+        public List<CarritoDetalleDTO> detalles { get; set; }
+    }
+}
