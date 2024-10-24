c5abd6a04cd2d7bd3f7ee9537821361ae84592d7
Gabriel <capaygabriel98@gmail.com>
2024-10-23 22:21:55 -0300

    Modelo DetalleCarrito

---
+++ ProyectoTaller/CModelos/DetalleCarrito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CModelos
+{
+    public class DetalleCarrito
+    {
+        public int cantitad {  get; set; }
+        public decimal Subtotal { get; set; }
+        public Producto producto { get; set; }
+    }
+}
