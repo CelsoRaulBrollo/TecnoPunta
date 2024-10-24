e7f16145c45cc5d10dba10d4d4e64d5856112bb1
Gabriel <capaygabriel98@gmail.com>
2024-10-23 22:22:10 -0300

    CapaNegocio Carrito

---
+++ ProyectoTaller/CNegocio/CarritoNegocio.cs
@@ -0,0 +1,27 @@
+﻿using ProyectoTaller.CDatos;
+using ProyectoTaller.CModelos;
+using ProyectoTaller.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.CNegocio
+{
+    public class CarritoNegocio
+    {
+        private CarritoDatos carritoDatos;
+
+        public CarritoDTO cargarCarito(int dniVendedor)
+        {
+            CarritoDTO carritoRespuesta = new CarritoDTO();
+            carritoDatos = new CarritoDatos();
+
+
+
+            return carritoRespuesta;
+        }
+
+    }
+}
