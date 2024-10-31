9bb1fc604731bb53e55fa688ae206351d59aed58
Gabriel <capaygabriel98@gmail.com>
2024-10-30 21:18:06 -0300

    imprimir

---
+++ ProyectoTaller/Questpdf/InvoiceDocument.cs
@@ -0,0 +1,41 @@
+﻿using ProyectoTaller.CModelos;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoTaller.Questpdf
+{
+    internal class VentaDocument: IDocument
+    {
+        public Venta venta { get; }
+
+        public VentaDocument(Venta model)
+        {
+            venta = model;
+        }
+
+        public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+        public DocumentSettings GetSettings() => DocumentSettings.Default;
+
+
+        public void Compose(IDocumentContainer container)
+        {
+            container
+                .Page(page =>
+                {
+                    page.Margin(50);
+
+                    page.Header().Height(100).Background(Colors.Grey.Lighten1);
+                    page.Content().Background(Colors.Grey.Lighten3);
+                    page.Footer().Height(50).Background(Colors.Grey.Lighten1);
+                });
+        }
+
+
+    }
+}
